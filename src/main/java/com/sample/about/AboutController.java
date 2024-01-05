af59d602660975be902cb05b19f80a81804f06c8
RosieOh <20172207@gm.hannam.ac.kr>
2024-01-05 20:38:32 +0900

    👨‍💻feat : About Section 컨트롤러 완👨‍💻

--- src/main/java/com/sample/about/AboutController.java
+++ src/main/java/com/sample/about/AboutController.java
@@ -1,4 +1,37 @@
 package com.sample.about;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
 public class AboutController {
+
+    private final AboutService aboutService;
+
+    @GetMapping("/about")
+    public String getAbout(Model model) {
+        String content = aboutService.getRecentContent();
+        model.addAttribute("content", content);
+        return "page/about";
+    }
+
+    @GetMapping("/about_edit")
+    public String getAboutEdit(Model model) {
+        String content = aboutService.getRecentContent();
+        model.addAttribute("content", content);
+        return "page/about_edit";
+    }
+
+    @PostMapping("/about_edit")
+    @ResponseBody
+    public About postAbout(@RequestBody About about) {
+        return aboutService.insertAbout(about);
+    }
 }
